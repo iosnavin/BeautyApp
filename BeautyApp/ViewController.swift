850fabbeeaeb32cca7ffe707a832f86812202156
Naveen <ios.nreddy@gmail.com>
2022-11-10 21:56:30 +0530

    Initial Commit

---
+++ BeautyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeautyApp
+//
+//  Created by Naveen Reddy on 10/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
